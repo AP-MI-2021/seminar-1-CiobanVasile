a3afa987dfa12d33b7a97355abe7da32be1a7434
Cioban Vasile <v.cioban@yahoo.com>
2021-09-29 17:50:49 +0300

    rezolvat toate problemele

--- main.py
+++ main.py
@@ -25,11 +25,45 @@ def is_prime(nr):
 
 	return True
 
+def test_is_prime():
+	assert is_prime(-2) == False
+	assert is_prime(0) == False
+	assert is_prime(1) == False
+	assert is_prime(2) == True
+	assert is_prime(15) == False
+	assert is_prime(13) == True
+	assert is_prime(103) == True
+	assert is_prime(17*19) == False
+	assert is_prime(666013) == True
+
+
+
+def print_primes(lst):
+	for i in range(len(lst)):
+		if is_prime(lst[i]):
+			print(f'Numarul {lst[i]} este prim')
+		else:
+			print(f'Numarul {lst[i]} nu este prim')
+
+def get_nr_cu_cifrele_invers(nr):
+	'''
+	...
+	'''
+	inv = []
+	while nr:
+		inv.append(str(nr % 10))
+		nr = nr // 10
+
+	return ''.join(inv)
+
+
 def main():
 
 	while True:
 		print('1. Minimul a trei numere.')
 		print('2. Determinare daca un nr e prim.')
+		print('3. Afisare numerelor prime dintr-o lista.')
+		print('4. Afisarea unui numar cu cifrele in ordine inversa.')
 		print('x. Exit')
 		optiune = input('Alegeti o optiune: ')
 		if optiune == '1':
@@ -45,9 +79,20 @@ def main():
 				print('Numarul dat este prim!')
 			else:
 				print('Numarul dat nu este prim.')
+		elif optiune == '3':
+			numere_str = input('Dati numerele separate prin spatiu: ')
+			numere_str_split = numere_str.split(' ')
+			numere_int = []
+			for nr_str in numere_str_split:
+				numere_int.append(int(nr_str))
+			print_primes(numere_int)
+		elif optiune == '4':
+			nr = int(input('Dati numarul: '))
+			print('Cifrele in ordine inversa sunt:', get_nr_cu_cifrele_invers(nr))
 		elif optiune == 'x':
 			break
 		else:
 			print('Optiune invalida.')
 
+test_is_prime()
 main()
